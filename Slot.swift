efc533a2f017edd44fe13987efbc3ec6adb907f7
goin18 <markobudal@gmail.com>
2015-02-11 18:53:15 +0100

    SlotMachine
    Simple iOS (swift) application slot machine.

---
+++ Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Marko Budal on 02/01/15.
+//  Copyright (c) 2015 Marko Budal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
